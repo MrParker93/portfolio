162f48de0d2f9ccb447fd1ca0448b227419594d2
Mparker93 <marcuseparker@hotmail.com>
2021-07-02 01:22:39 +0100

    Added contact form and enabled emails to be sent to my personal email address. (Beta phase, needs improving)

--- webpage/__init__.py
+++ webpage/__init__.py
@@ -1,6 +1,9 @@
 import os
 
 from flask import Flask
+from flask_mail import Message, Mail
+
+mail = Mail()
 
 
 def create_app(test_config=None):
@@ -10,6 +13,15 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'webpage.sqlite'),
     )
+
+    # Configure flask mail server settings
+    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_PORT"] = 465
+    app.config["MAIL_USE_SSL"] = True
+    app.config["MAIL_USERNAME"] = "marcuseparker93@gmail.com"
+    app.config["MAIL_PASSWORD"] = "Sh@nkZ123"
+
+    mail.init_app(app)
 
     if test_config is None:
         # Load the instance config, if it exists, when not testing
--- webpage/application.py
+++ webpage/application.py
@@ -4,9 +4,13 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
+from flask_mail import Message, Mail
+from .forms import ContactForm
 from webpage.db import get_db
 
+
 bp = Blueprint("application", __name__)
+mail = Mail()
 
 
 @bp.route("/")
@@ -38,7 +42,29 @@ def achievements():
     # Display achievements page
     return render_template("achievements.html")
 
-@bp.route("/contact")
+
+@bp.route("/contact", methods=["GET", "POST"])
 def contact():
     # Display contact form
-    return render_template("contact.html")
+    form = ContactForm()
+
+    # User reached route via POST (as by submitting a form etc)
+    if request.method == "POST":
+        # Ensure all fields are submitted
+        if not form.validate():
+            flash("All fields are required.", category="error")
+            return render_template("contact.html", form=form)
+        else:
+            msg = Message(form.subject.data, sender="marcuseparker93@gmail.com",
+                          recipients=["marcuseparker@hotmail.com"])
+            msg.body = """
+            %s
+            From %s <%s>
+            """ % (form.message.data, form.name.data, form.email.data)
+            mail.send(msg)
+
+            return render_template("contact.html", success=True)
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("contact.html", form=form)
